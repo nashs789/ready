b37964c5801f452c28e644170e1191ea08f1352c
nashs789 <nashs789@naver.com>
2024-06-23 17:45:29 +0900

    feat: 백준 알고리즘 분류 페이지 크롤링 연습

--- src/main/java/com/example/ready/controller/testController.java
+++ src/main/java/com/example/ready/controller/testController.java
@@ -1,9 +1,15 @@
 package com.example.ready.controller;
 
+import com.example.ready.entity.Algorithm;
 import lombok.RequiredArgsConstructor;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,8 +19,20 @@ public class testController {
 
     @GetMapping("/")
     public void test() {
-        chromeDriver.get("https://kream.co.kr/");
+        chromeDriver.get("https://www.acmicpc.net/problem/tags");
 
-        System.out.println(chromeDriver.getTitle());
+        chromeDriver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+        List<WebElement> rows = chromeDriver.findElements(By.cssSelector(".table-responsive tbody tr"));
+
+        rows.forEach(row -> {
+            List<WebElement> tds = row.findElements(By.tagName("td"));
+            WebElement firstTd = tds.get(0);
+            WebElement secondTd = tds.get(1);
+            String url = firstTd.findElement(By.tagName("a")).getAttribute("href");
+
+            Algorithm al = new Algorithm(firstTd.getText(), secondTd.getText(), url);
+            System.out.println(al);
+        });
     }
 }
---
+++ src/main/java/com/example/ready/entity/Algorithm.java
@@ -0,0 +1,12 @@
+package com.example.ready.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@AllArgsConstructor
+public class Algorithm {
+    String korName;
+    String engName;
+    String url;
+}
