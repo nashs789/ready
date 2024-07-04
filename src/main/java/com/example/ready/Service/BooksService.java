52db90db90181935a67d5a35c7ea3e8a8f4133d8
nashs789 <nashs789@naver.com>
2024-07-05 01:33:26 +0900

    feat: 연습 사이트 책 카테고리 & 책 정보 크롤링중

--- src/main/java/com/example/ready/Service/BooksService.java
+++ src/main/java/com/example/ready/Service/BooksService.java
@@ -7,7 +7,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
-import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -17,12 +18,39 @@ public class BooksService {
 
     public void getBooks() {
         chromeDriver.get("https://books.toscrape.com/");
-        chromeDriver.manage()
-                    .timeouts()
-                    .implicitlyWait(Duration.ofMillis(500));
+        waiting();
 
         WebElement side_categories = chromeDriver.findElement(By.className("side_categories"));
-        System.out.println(side_categories.getText());
-        String[] categories = side_categories.getText().split("\n");
+        String[] categories = side_categories.getText().split("\n");  // 카테고리 text 배열
+
+        List<String> categoryUrls = side_categories.findElement(By.cssSelector("ul ul"))
+                                                   .findElements(By.tagName("a"))
+                                                   .stream()
+                                                   .map(e -> e.getAttribute("href"))
+                                                   .collect(Collectors.toList());
+
+        for(String url : categoryUrls) {
+            chromeDriver.get(url);  // 페이지 이동
+            waiting();  // 페이지 렌더링 대기
+
+            String currentCategory = chromeDriver.findElement(By.className("page-header"))
+                                                 .getText();
+
+            if(currentCategory.startsWith("Sequential Art")) {  // 4번째 카테고리 종료 하드코딩
+                break;
+            }
+
+            List<WebElement> booksInfo = chromeDriver.findElement(By.tagName("section"))
+                                                     .findElement(By.tagName("ol"))
+                                                     .findElements(By.tagName("li"));
+
+            booksInfo.stream().forEach(e -> {
+                System.out.println(e.getText());
+            });
+        }
+    }
+
+    public void waiting() {
+        chromeDriver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
     }
 }
---
+++ src/main/java/com/example/ready/enums/Stars.java
@@ -0,0 +1,4 @@
+package com.example.ready.enums;
+
+public enum Stars {
+}
