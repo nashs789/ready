c382f8acb0410a449b567d3ad3f21ac7e84cb8da
nashs789 <nashs789@naver.com>
2024-07-08 01:26:12 +0900

    feat: 스크래핑 실습 마무리

--- src/main/java/com/example/ready/Service/BooksService.java
+++ src/main/java/com/example/ready/Service/BooksService.java
@@ -1,5 +1,6 @@
 package com.example.ready.Service;
 
+import com.example.ready.enums.Stars;
 import lombok.RequiredArgsConstructor;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -7,6 +8,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -29,6 +32,8 @@ public class BooksService {
                                                    .map(e -> e.getAttribute("href"))
                                                    .collect(Collectors.toList());
 
+        List<String> allBooksDetailUrls = new ArrayList<>();
+
         for(String url : categoryUrls) {
             chromeDriver.get(url);  // 페이지 이동
             waiting();  // 페이지 렌더링 대기
@@ -40,13 +45,51 @@ public class BooksService {
                 break;
             }
 
-            List<WebElement> booksInfo = chromeDriver.findElement(By.tagName("section"))
-                                                     .findElement(By.tagName("ol"))
-                                                     .findElements(By.tagName("li"));
+            List<WebElement> detailUrls = chromeDriver.findElement(By.tagName("section")).findElements(By.className("image_container"));
 
-            booksInfo.stream().forEach(e -> {
-                System.out.println(e.getText());
-            });
+            // 책 상세정보 url 저장
+            for(WebElement detailUrl : detailUrls) {
+                String bookDetailUrl = detailUrl.findElement(By.tagName("a")).getAttribute("href");
+
+                allBooksDetailUrls.add(bookDetailUrl);
+            }
+
+            // 다음 페이지가 존재 하면 탐색 url 에 추가
+            try {
+                WebElement next = chromeDriver.findElement(By.className("next"));
+                String nextPage = next.findElement(By.tagName("a")).getAttribute("href");
+
+                //categoryUrls.add(nextPage);
+            } catch (Exception e) {
+
+            }
+        }
+
+        // 책 상세정보 데이터 수집
+        for(String url : allBooksDetailUrls) {
+            chromeDriver.get(url);  // 페이지 이동
+            waiting();  // 페이지 렌더링 대기
+
+            WebElement product_main = chromeDriver.findElement(By.className("product_main"));
+
+            String title = product_main.findElement(By.cssSelector("h1")).getText();
+            Stars stars = Stars.getStars(product_main.findElement(By.className("star-rating")).getAttribute("class").split(" ")[1]);
+            List<WebElement> tds = chromeDriver.findElement(By.cssSelector("table")).findElements(By.cssSelector("td"));
+
+            String upc = tds.get(0).getText();
+            String type = tds.get(1).getText();
+            String price = tds.get(2).getText();
+            String priceWithTax = tds.get(3).getText();
+            String tax = tds.get(4).getText();
+            String availability = tds.get(5).getText();
+            String reviews = tds.get(6).getText();
+
+            System.out.println("==============================");
+            System.out.println("title = " + title);
+            System.out.println("stars = " + stars);
+            System.out.println("upc = " + upc);
+            System.out.println("type = " + type);
+            System.out.println("price = " + price);
         }
     }
 
--- src/main/java/com/example/ready/enums/Stars.java
+++ src/main/java/com/example/ready/enums/Stars.java
@@ -1,4 +1,29 @@
 package com.example.ready.enums;
 
 public enum Stars {
+    ONE("One"),
+    TWO("Two"),
+    THREE("Three"),
+    FOUR("Four"),
+    FIVE("Five");
+
+    Stars(String star) {
+        this.star = star;
+    }
+
+    private String star;
+
+    public static Stars getStars(String star) {
+        for(Stars s : Stars.values()) {
+            if(s.getStar().equals(star)) {
+                return s;
+            }
+        }
+
+        throw new IllegalArgumentException("No Such a Matched Enum with" + star);
+    }
+
+    public String getStar() {
+        return star;
+    }
 }
